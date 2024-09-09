6c40bf128734b46a37c94f8dfa70c94826fa3c00
sandroweslley <sandroweslley@gmail.com>
2024-09-09 20:45:10 -0300

    Create exerc 05.py

---
+++ exercícios lógica 2/exerc 05.py
@@ -0,0 +1,7 @@
+soma = 0
+while True:
+    numero = int(input("digite um numero (ou 0 para sair):")) 
+    if numero == 0:
+        break
+    soma += numero
+print("A soma dos numeros digitados é:", soma)
