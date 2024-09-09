9d1cbb2d2cffb457c93ae1fceef4b6406e9d0fcd
sandroweslley <sandroweslley@gmail.com>
2024-09-09 20:43:02 -0300

    Create exerc 04.py

---
+++ exercícios lógica 2/exerc 04.py
@@ -0,0 +1,7 @@
+temperatura=int(input("qual a temperatura da sua cidade?"))
+if temperatura >30:
+  print("está quente")
+elif temperatura <15:
+    print("está frio")
+else:
+      print("está agradvel")
