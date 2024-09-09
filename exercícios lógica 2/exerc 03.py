2f3fe41136d44f28470c7e4e002cc87cc3ec830d
sandroweslley <sandroweslley@gmail.com>
2024-09-09 20:17:25 -0300

    Create exerc 03.py

---
+++ exercícios lógica 2/exerc 03.py
@@ -0,0 +1,3 @@
+filme = input("Qual é seu filme favorito?")
+ano = input("Qual foi o ano do lançamento desse filme?")
+print("Agora eu sei que seu filme preferido é" + filme + "! e que ele foi lançado em" +ano)
