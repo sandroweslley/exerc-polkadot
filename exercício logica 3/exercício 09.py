57d7eb5d448da0ac6cbf8cbc4d90a9fc61ac3d2e
sandroweslley <sandroweslley@gmail.com>
2024-09-16 10:53:03 -0300

    Create exercício 09.py

---
+++ exercício logica 3/exercício 09.py
@@ -0,0 +1,7 @@
+num1=input("digite o primeiro número:")
+num2=input("digite o segundo número:")
+num3=input("digite o terceiro número:")
+
+lista=[num1,num2,num3]
+lista.sort()
+print("os números em ordem crescente:",lista)
