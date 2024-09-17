e3d28494b4b8ff7f61991cf6e05004c21d13bf91
sandroweslley <sandroweslley@gmail.com>
2024-09-17 09:41:11 -0300

    Update exercício 18.py

--- exercício logica 3/exercício 18.py
+++ exercício logica 3/exercício 18.py
@@ -1,11 +1,4 @@
-n = int(input("digite um número: "))
-d = 1
-soma = 0
-while d <= 1/2:
-  if n % d == 0:
-     soma += d
-     d += 1
-if n == soma :
-   print(f"{n} é perfeito")
-else :
-   print(f"{n} não é perfeito")
+frase = input("Digite uma frase: ")
+palavras = frase.split()
+contagem = len(palavras)
+print("Há", contagem, "palavras na sua frase.")
