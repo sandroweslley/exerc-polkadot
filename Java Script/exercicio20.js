daffe8f3c63c061f916a75d9cea657fcbaa60817
sandroweslley <sandroweslley@gmail.com>
2024-10-07 08:54:39 -0300

    Create exercicio20.js

---
+++ Java Script/exercicio20.js
@@ -0,0 +1,10 @@
+let digitos = 123
+function somarDigitos(num) {
+    let separar = num.toString().split("").map(Number)
+    let armazenaNum = 0;
+    for(let i = 0; i < separar.length; i++) {
+        armazenaNum += separar[i]
+    }
+    return console.log(`A soma dos digitos '${digitos}' é: ${armazenaNum}`);
+}
+somarDigitos(digitos)
